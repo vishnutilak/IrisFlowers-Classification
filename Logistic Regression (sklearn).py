70075b8ec2ab28c2a1c85701a0e4b37eba8a218e
Vishnu Tilak <29648840+vishnutilak@users.noreply.github.com>
2020-09-26 22:46:15 +0530

    Add files via upload

---
+++ Logistic Regression (sklearn).py
@@ -0,0 +1,22 @@
+import pandas as pd
+from matplotlib import pyplot as plt
+from sklearn.model_selection import train_test_split
+from sklearn.metrics import classification_report, confusion_matrix, accuracy_score
+from sklearn.linear_model import LogisticRegression
+
+data_set_path= "Iris-dataset.csv"
+names = ['sepal-length', 'sepal-width', 'petal-length', 'petal-width', 'class']
+data_set = pd.read_csv(data_set_path,names=names)
+
+array = data_set.values
+X = array[:,0:4] #all rows-data, columns 0,1,2,3
+y = array[:,4] #all rows of data, column 4-last column-result column
+X_train, X_test, Y_train, Y_test = train_test_split(X, y, test_size=0.20, random_state=1, shuffle=True)
+
+model = LogisticRegression(solver='liblinear', multi_class='ovr')
+model.fit(X_train, Y_train)
+predictions = model.predict(X_test)
+
+print(accuracy_score(Y_test, predictions))
+print(confusion_matrix(Y_test, predictions))
+print(classification_report(Y_test, predictions))
